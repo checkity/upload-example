e7ed40e18d18d53a0be52b9ce3638797bfc17203
Stephan Erlank <stephan.erlank@checkity.io>
2021-11-24 17:18:30 +1300

    adding untested changes

--- src/main/java/checkity/upload/Example.java
+++ src/main/java/checkity/upload/Example.java
@@ -3,6 +3,11 @@ package checkity.upload;
 public class Example {
 
     public boolean isEven(int number) {
+        if (number < 10) {
+            System.out.printf("%d is less than 10%n", number);
+        } else if (number > 10) {
+            System.out.printf("%d is greater than 10%n", number);
+        }
         if (number % 2 == 0) {
             System.out.printf("%d is even%n", number);
             return true;
